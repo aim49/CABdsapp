b725402893616dce4ae08d30691718448a97ece9
R3D <aimmak49@gmail.com>
2019-05-13 13:47:56 +0200

    CABdsapp Instance2

---
+++ app/src/main/java/com/darkside/cab/DriverLoginRegisterActivity.java
@@ -0,0 +1,13 @@
+package com.darkside.cab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverLoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login_register);
+    }
+}
